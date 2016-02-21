fdbd43e5ea65d2414ea57d11fcbcf0cda9ea4c40
karupanerura <karupa@cpan.org>
2016-02-21 18:26:32 +0900

    branchを指定できるようにしてみる

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,21 @@ set -ux
 
 cd $(dirname $0)
 
+branch="$1"
 if [ `uname -s` = 'Darwin' ]; then
     for server in "isucon5-final-app1" "isucon5-final-app2" "isucon5-final-app3"; do
-        gcloud compute --project "isucon5-qualifier-oniyanma" ssh --zone "asia-east1-c" $server -- sudo -H /home/isucon/isucon5-final/deploy.sh
+        gcloud compute --project "isucon5-qualifier-oniyanma" ssh --zone "asia-east1-c" $server -- sudo -H /home/isucon/isucon5-final/deploy.sh $branch
     done
 elif [ `whoami` != 'isucon' ]; then
     sudo -u isucon -H git reset --hard
     sudo -u isucon -H git pull
-    sudo -u isucon -H $PWD/deploy.sh
+    sudo -u isucon -H $PWD/deploy.sh $branch
 else
     git reset --hard
     git pull
+    if [ $branch != "" ]; then
+        git checkout $branch
+        git pull
+    fi
     ./config/install.sh
 fi
