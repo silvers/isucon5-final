bfd7ce0394e367a7354c9bdc894a17f3c65f9b1d
karupanerura <karupa@cpan.org>
2016-02-21 15:18:51 +0900

    ちょいなおす

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,10 @@ cd $(dirname $0)
 
 if [ `uname -s` = 'Darwin' ]; then
     for servers in "isucon5-final-app1" "isucon5-final-app2" "isucon5-final-app3"; do
-        gcloud compute --project "isucon5-qualifier-oniyanma" ssh --zone "asia-east1-c" "isucon5-final-app1" sudo -H /home/isucon/isucon5-final/deploy.sh
+        gcloud compute --project "isucon5-qualifier-oniyanma" ssh --zone "asia-east1-c" "isucon5-final-app1" -- sudo -H /home/isucon/isucon5-final/deploy.sh
     done
 elif [ `whoami` != 'isucon' ]; then
-    sudo -u isucon -H deploy.sh
+    sudo -u isucon -H $PWD/deploy.sh
 else
     git reset --hard
     git pull
