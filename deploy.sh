ce1cf56f2e4724e8a31b297507699e93505ee327
karupanerura <karupa@cpan.org>
2016-02-21 15:44:46 +0900

    app1以外にも配って！！！

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ set -ux
 cd $(dirname $0)
 
 if [ `uname -s` = 'Darwin' ]; then
-    for servers in "isucon5-final-app1" "isucon5-final-app2" "isucon5-final-app3"; do
-        gcloud compute --project "isucon5-qualifier-oniyanma" ssh --zone "asia-east1-c" "isucon5-final-app1" -- sudo -H /home/isucon/isucon5-final/deploy.sh
+    for server in "isucon5-final-app1" "isucon5-final-app2" "isucon5-final-app3"; do
+        gcloud compute --project "isucon5-qualifier-oniyanma" ssh --zone "asia-east1-c" $server -- sudo -H /home/isucon/isucon5-final/deploy.sh
     done
 elif [ `whoami` != 'isucon' ]; then
     sudo -u isucon -H $PWD/deploy.sh
